d9a009052f61afaef6e3d1debcf4f28bfca3faee
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-08-30 17:17:28 -0400

    Started initial structure and cmake

---
+++ application/include/asmlgen/application/config/assertion.h
@@ -0,0 +1,11 @@
+ifndef ASMLGEN_APPLICATION_ASSERTION_VERSION_H
+#define ASMLGEN_APPLICATION_ASSERTION_VERSION_H
+
+#ifdef NDEBUG
+#define ASSERT(expression, message) ((void)0)
+#else
+#include <cassert>
+#define ASSERT(expression, message) assert(((void)(message), (expression)))
+#endif
+
+#endif
---
+++ application/source/asmlgen/application/dummy.cpp
@@ -0,0 +1,6 @@
+/// Dummy file to get project to compile
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Project is compiling!" << std::endl;
+}
