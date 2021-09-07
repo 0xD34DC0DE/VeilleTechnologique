ce1e5d0434f2027e620bcf33617f059d434b48c5
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-06 22:28:01 -0400

    Added parsing helper

--- application/source/asmlgen/application/dataset/json_parsing_helpers.cpp
+++ application/source/asmlgen/application/dataset/json_parsing_helpers.cpp
@@ -1,9 +1,30 @@
 #include "asmlgen/application/dataset/json_parsing_helpers.h"
 
+#include "rapidjson/error/en.h"
+
+#include <iostream>
+
 namespace dataset
 {
 
 // TODO replace assert with debug only assert
+
+rapidjson::Document* GetParsedDocument(const std::vector<char>& json)
+{
+  if (json.empty()) { return nullptr; }
+
+  auto* document = new rapidjson::Document;
+
+  if (document->Parse(json.data()).HasParseError())
+  {
+    std::cerr << "Json document parsing error: \n"
+              << "Error(offset " << static_cast<unsigned>(document->GetErrorOffset()) << "):\n\t"
+              << GetParseError_En(document->GetParseError()) << std::endl;
+    return nullptr;
+  }
+
+  return document;
+}
 
 template<>
 uint64_t ParseValueField<uint64_t>(const rapidjson::Value& jsonValueRef, const char* fieldName)
