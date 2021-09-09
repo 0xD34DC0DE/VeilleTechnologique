cdf63dd90052ca0011521a1b2c62ac1d80d1ad9e
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-08 21:16:32 -0400

    try out new features

--- sandbox/parser/parser.cpp
+++ sandbox/parser/parser.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 
+#include "asmlgen/application/dataset/coco_json_parser.h"
+#include "asmlgen/application/dataset/file_reader.h"
+
+#define SFML_STATIC
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+
+#include <memory>
+
+using namespace dataset;
 
 int main(int, char**)
 {
-  std::cout << "Project is compiling!" << std::endl;
+
+  std::cout << "Loading json file" << std::endl;
+  std::vector<char> json =
+    FileReader::ReadFile(R"(C:\Users\0xD34DC0DE\PycharmProjects\PersonSegmentation\instances_val2017.json)");
+  std::cout << "Json loaded: " << json.size() << " bytes" << std::endl;
+
+  std::cout << "Entry loading entries" << std::endl;
+
+  std::unique_ptr<DatasetJsonParser<TransientCocoEntry>> coco_json_parser_ptr = std::make_unique<CocoJsonParser>();
+  uint64_t entry_count = coco_json_parser_ptr->LoadJson(json);
+
+  std::cout << "Entry loaded: " << entry_count << std::endl;
+
+  sf::Http http("http://images.cocodataset.org/");
+  sf::Http::Request request("/val2017/000000114907.jpg", sf::Http::Request::Get);
+  sf::Http::Response response = http.sendRequest(request, sf::seconds(2));
 
   auto* window = new sf::RenderWindow(sf::VideoMode(512, 512), "Hello world");
 
