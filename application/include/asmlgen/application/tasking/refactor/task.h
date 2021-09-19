5792955163ac4085e6607ee83ba21e3880ab03fc
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-19 00:23:16 -0400

    started reverse engineering chained function calls

--- application/include/asmlgen/application/tasking/refactor/task.h
+++ application/include/asmlgen/application/tasking/refactor/task.h
@@ -26,56 +26,56 @@ R ret(R (C::*)(A...));
 template<typename InputType, typename OutputType>
 class Task
 {
-public:
-  Task() = default;
-  virtual ~Task() = default;
-
-  template<template<typename, typename> typename Task_T, typename T>
-  Task<OutputType, T> Then(Task_T<OutputType, T> a)
-  {
-    f_ = [a] { Task_T<OutputType, T> v = a.Start({}); };
-    //    next_task_ = std::move(next);
-    //    return next_task_;
-    return Task<OutputType, T> {};
-  }
-
-  void Start(InputType input);
-  {
-    Work(input);
-    Done();
-  }
-
-  virtual void Work(InputType& input) = 0;
-
-  void Done()
-  {
-    if (next_task_.has_value()) { next_task_.value().Start(output_); }
-  }
-
-  auto GetResultFromLast()
-  {
-    if (next_task_.has_value()) { return GetResultFromLast(); }
-    else
-    {
-      return output_;
-    }
-  }
-
-protected:
-  OutputType output_;
-
-private:
-  bool has_next_ = false;
-  std::function<void()> f_;
-
-  auto GetResultFromLast(Task& next)
-  {
-    if (next.next_task_.has_value()) { return GetResultFromLast(next.next_task_.value()); }
-    else
-    {
-      output_;
-    }
-  }
+  // public:
+  //   Task() = default;
+  //   virtual ~Task() = default;
+  //
+  //   template<template<typename, typename> typename Task_T, typename T>
+  //   Task<OutputType, T> Then(Task_T<OutputType, T> a)
+  //   {
+  //     f_ = [a] { Task_T<OutputType, T> v = a.Start({}); };
+  //     //    next_task_ = std::move(next);
+  //     //    return next_task_;
+  //     return Task<OutputType, T> {};
+  //   }
+  //
+  //   void Start(InputType input);
+  //   //  {
+  //   //    Work(input);
+  //   //    Done();
+  //   //  }
+  //   //
+  //   //  virtual void Work(InputType& input) = 0;
+  //
+  //   //  void Done()
+  //   //  {
+  //   //    if (next_task_.has_value()) { next_task_.value().Start(output_); }
+  //   //  }
+  //   //
+  //   //  auto GetResultFromLast()
+  //   //  {
+  //   //    if (next_task_.has_value()) { return GetResultFromLast(); }
+  //   //    else
+  //   //    {
+  //   //      return output_;
+  //   //    }
+  //   //  }
+  //
+  // protected:
+  //   OutputType output_;
+  //
+  // private:
+  //   bool has_next_ = false;
+  //   std::function<void()> f_;
+  //
+  //   auto GetResultFromLast(Task& next)
+  //   {
+  //     if (next.next_task_.has_value()) { return GetResultFromLast(next.next_task_.value()); }
+  //     else
+  //     {
+  //       output_;
+  //     }
+  //   }
 };
 
 } // namespace tasking
--- sandbox/refactored_task/main.cpp
+++ sandbox/refactored_task/main.cpp
@@ -5,28 +5,54 @@
 
 using namespace tasking;
 
-class DownloadTask : public Task<bool, long>
+// class DownloadTask : public Task<bool, long>
+//{
+// public:
+//   void Work(std::string&) {}
+// };
+//
+// class DecompressTask : public Task<long, char>
+//{
+// public:
+//   void Work(std::vector<int>&) {}
+// };
+//
+// class WriteTask : public Task<char, unsigned int>
+//{
+// public:
+//   void Work(int&) {}
+// };
+
+template<typename... Types>
+struct InputTypeList
 {
-public:
-  void Work(std::string&) {}
+  using type = typename std::tuple_element<0, std::tuple<Types...>>::type::InputType;
 };
 
-class DecompressTask : public Task<long, char>
+template<typename... Types>
+struct OutputTypeList
 {
-public:
-  void Work(std::vector<int>&) {}
+  using type = typename std::tuple_element<sizeof...(Types) - 1, std::tuple<Types...>>::type::InputType;
 };
 
-class WriteTask : public Task<char, unsigned int>
+template<typename Type>
+typename Type::OutputType CallNextRecursive(typename Type::InputType input, Type next)
 {
-public:
-  void Work(int&) {}
-};
+  return next.Apply(input);
+}
+
+template<typename Type, typename... Types>
+typename std::enable_if<(sizeof...(Types) > 0), typename OutputTypeList<Type, Types...>::type>::type CallNextRecursive(
+  typename InputTypeList<Type, Types...>::type input, Type next, Types... nexts)
+{
+  auto output = next.Apply(input);
+  return CallNextRecursive(output, nexts...);
+}
 
 int main(int, char**)
 {
 
-  auto a = DownloadTask {};
-
-  a.Then(DecompressTask {});
+  //  auto a = DownloadTask {};
+  //
+  //  a.Then(DecompressTask {});
 }
