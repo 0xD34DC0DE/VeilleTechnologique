a46966c56225e5530b294402f324faae686ac01d
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-20 21:35:00 -0400

    Optimized chaining code using constexpr if

--- sandbox/refactored_task/main.cpp
+++ sandbox/refactored_task/main.cpp
@@ -23,97 +23,6 @@ using namespace tasking;
 // public:
 //   void Work(int&) {}
 // };
-// https://gist.github.com/leopoldcambier/1f5b92b0afc8754e40f7faa6b9bd8afc
-
-template<typename... Types>
-struct InputTypeList
-{
-  using type = typename std::tuple_element<0, std::tuple<Types...>>::type::InputType;
-};
-
-template<typename... Types>
-struct OutputTypeList
-{
-  using type = typename std::tuple_element<sizeof...(Types) - 1, std::tuple<Types...>>::type::OutputType;
-};
-
-template<typename Type>
-typename Type::OutputType CallNextRecursive(typename Type::InputType input, Type next)
-{
-  return next.Call(input);
-}
-
-template<typename Type, typename... Types>
-typename std::enable_if<(sizeof...(Types) > 0), typename OutputTypeList<Type, Types...>::type>::type CallNextRecursive(
-  typename InputTypeList<Type, Types...>::type input, Type next, Types... nexts)
-{
-  auto output = next.Call(input);
-  return CallNextRecursive(output, nexts...);
-}
-
-template<typename InType, typename TransformerTuple, std::size_t... Int>
-constexpr auto ChainCallHelper(InType& in, TransformerTuple& tranformer_tuple, std::index_sequence<Int...>)
-{
-  return CallNextRecursive(in, std::get<Int>(tranformer_tuple)...);
-}
-
-template<typename InType, typename TranformerTuple>
-constexpr auto ChainCallTuple(InType& in, TranformerTuple& tranformer_tuple)
-{
-  return ChainCallHelper(in, tranformer_tuple, std::make_index_sequence<std::tuple_size<TranformerTuple>::value> {});
-}
-
-template<typename... Calls>
-struct ChainedCalls
-{
-  using InputType = typename InputTypeList<Calls...>::type;
-  using OutputType = typename OutputTypeList<Calls...>::type;
-
-  std::tuple<Calls...> calls;
-
-  explicit ChainedCalls(Calls... calls) : calls(std::tuple<Calls...>(calls...)) {};
-
-  OutputType Call(InputType input)
-  {
-    return ChainCallTuple(input, calls);
-  }
-};
-
-template<typename... Calls>
-auto MakeChainedCalls(Calls... calls)
-{
-  return ChainedCalls<Calls...>(calls...);
-}
-
-struct callable1
-{
-  std::string Call(bool b)
-  {
-    if (b) { return "1"; }
-    else
-    {
-      return "A";
-    }
-  }
-
-  using InputType = bool;
-  using OutputType = std::string;
-};
-
-struct callable2
-{
-  int Call(std::string s)
-  {
-    if (s == "1") { return 1; }
-    else
-    {
-      return 0;
-    }
-  }
-
-  using InputType = std::string;
-  using OutputType = int;
-};
 
 // REFACTOR
 
@@ -136,11 +45,23 @@ constexpr typename Chainable::OutputType CallNext(typename Chainable::InputType&
   return function.Call(input);
 }
 
+template<typename Chainable>
+constexpr typename Chainable::OutputType EndChain(typename Chainable::InputType& input, Chainable function)
+{
+  return function.Call(input);
+}
+
 template<typename Chainable, typename... Chainables>
-constexpr auto CallNext(typename Chainable::InputType& input, Chainable function, Chainables... next_functions)
+constexpr typename GetOutputTypeOfLast<Chainable, Chainables...>::OutputType CallNext(
+  typename Chainable::InputType& input, Chainable function, Chainables... next_functions)
 {
   auto output = function.Call(input);
-  CallNext(output, next_functions...);
+
+  if constexpr (sizeof...(Chainables) > 0) { return CallNext(output, next_functions...); }
+  else
+  {
+    return EndChain(output, next_functions...);
+  }
 }
 
 template<typename InputType, typename ChainableTuple, std::size_t... Indices>
@@ -219,3 +140,5 @@ int main(int, char**)
   std::cout << chain.Start(true) << std::endl;
   std::cout << chain.Start(false) << std::endl;
 }
+
+// 9:17
