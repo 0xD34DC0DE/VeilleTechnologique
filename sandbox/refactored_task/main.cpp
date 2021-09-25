345ddf4dae94dc3d200f9644682fefa8c2634969
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-25 18:30:37 -0400

    Prepare sandbox for TaskManager

--- sandbox/refactored_task/main.cpp
+++ sandbox/refactored_task/main.cpp
@@ -1,65 +1,7 @@
-#include "asmlgen/application/tasking/refactor/task.h"
-
-#include <iostream>
-#include <string>
-#include <vector>
+#include "asmlgen/application/tasking/refactor/task_manager.h"
 
 using namespace tasking;
 
-// class DownloadTask : public Task<bool, long>
-//{
-// public:
-//   void Work(std::string&) {}
-// };
-//
-// class DecompressTask : public Task<long, char>
-//{
-// public:
-//   void Work(std::vector<int>&) {}
-// };
-//
-// class WriteTask : public Task<char, unsigned int>
-//{
-// public:
-//   void Work(int&) {}
-// };
-
-// REFACTOR
-
-// struct chainable1 : MakeChainObj<chainable1, std::string, bool>
-//{
-//   std::string Call(bool b)
-//   {
-//     if (b) { return "1"; }
-//     else
-//     {
-//       return "A";
-//     }
-//   }
-// };
-//
-// struct chainable2 : MakeChainObj<chainable1, int, std::string>
-//{
-//   int Call(std::string s)
-//   {
-//     if (s == "1") { return 1; }
-//     else
-//     {
-//       return 0;
-//     }
-//   }
-// };
-//
-// int main(int, char**)
-//{
-//   auto f1 = chainable1();
-//   auto f2 = chainable2();
-//   f1.verifier_();
-//
-//   auto chain = MakeChained(f1, f2);
-//
-//   std::cout << chain.Start(true) << std::endl;
-//   std::cout << chain.Start(false) << std::endl;
-// }
+int main(int, char**) {}
 
 // 9:17 to 12:00
