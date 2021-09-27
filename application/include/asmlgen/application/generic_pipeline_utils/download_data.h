7730955a053c912a335ddb04e55b6a92076e7272
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-26 20:43:41 -0400

    Added DownloadData Pipeline Data

---
+++ application/include/asmlgen/application/generic_pipeline_utils/download_data.h
@@ -0,0 +1,56 @@
+#ifndef ASMLGEN_DOWNLOAD_DATA_H
+#define ASMLGEN_DOWNLOAD_DATA_H
+
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace pipeline
+{
+
+class DownloadData
+{
+public:
+  DownloadData() = default;
+  DownloadData(std::string url, std::string write_filepath);
+
+  [[nodiscard]] const std::string& GetUrl() const noexcept;
+
+  [[nodiscard]] const std::string& GetHost() const noexcept;
+
+  [[nodiscard]] const std::string& GetResource() const noexcept;
+
+  [[nodiscard]] std::size_t GetDownloadSize() const noexcept;
+
+  [[nodiscard]] const std::string& GetOutputFilepath() const noexcept;
+
+  [[nodiscard]] const std::vector<uint8_t>& GetData() const noexcept;
+
+  [[nodiscard]] std::vector<uint8_t>&& GetDataRvalRef() noexcept;
+
+  void SetDownloadSize(std::size_t download_size) noexcept;
+
+  void SetData(const std::vector<uint8_t>& data);
+
+  void SetData(std::vector<uint8_t>&& data) noexcept;
+
+  void MarkFailed() noexcept;
+
+  bool HasFailed() const noexcept;
+
+private:
+  std::string url_;
+  std::string host_;
+  std::string resource_;
+  std::size_t download_size_;
+  std::string output_filepath_;
+
+  std::vector<uint8_t> data_;
+
+  bool failed_;
+
+  void SetHostAndResource(const std::string& url);
+};
+
+} // namespace pipeline
+#endif // ASMLGEN_DOWNLOAD_DATA_H
---
+++ application/source/asmlgen/application/generic_pipeline_utils/download_data.cpp
@@ -0,0 +1,81 @@
+#include "asmlgen/application/generic_pipeline_utils/download_data.h"
+
+namespace pipeline
+{
+
+DownloadData::DownloadData(std::string url, std::string write_filepath)
+  : url_(std::move(url)), output_filepath_(std::move(write_filepath)), failed_(false)
+{
+  SetHostAndResource(url_);
+}
+
+const std::string& DownloadData::GetUrl() const noexcept
+{
+  return url_;
+}
+
+const std::string& DownloadData::GetHost() const noexcept
+{
+  return host_;
+}
+
+const std::string& DownloadData::GetResource() const noexcept
+{
+  return resource_;
+}
+
+std::size_t DownloadData::GetDownloadSize() const noexcept
+{
+  return download_size_;
+}
+
+const std::string& DownloadData::GetOutputFilepath() const noexcept
+{
+  return output_filepath_;
+}
+
+const std::vector<uint8_t>& DownloadData::GetData() const noexcept
+{
+  return data_;
+}
+
+void DownloadData::SetDownloadSize(std::size_t download_size) noexcept
+{
+  download_size_ = download_size;
+}
+
+void DownloadData::SetData(const std::vector<uint8_t>& data)
+{
+  data_ = data;
+}
+
+void DownloadData::SetData(std::vector<uint8_t>&& data) noexcept
+{
+  data_ = std::move(data);
+}
+
+void DownloadData::SetHostAndResource(const std::string& url)
+{
+  std::size_t protocol_end = url.find_first_of(':') + 3; // start of host is after "://" -> +3
+  std::size_t host_end = url.find_first_of('/', protocol_end);
+
+  host_ = url.substr(0, host_end);
+  resource_ = url.substr(host_end);
+}
+
+std::vector<uint8_t>&& DownloadData::GetDataRvalRef() noexcept
+{
+  return std::move(data_);
+}
+
+void DownloadData::MarkFailed() noexcept
+{
+  failed_ = true;
+}
+
+bool DownloadData::HasFailed() const noexcept
+{
+  return failed_;
+}
+
+} // namespace pipeline
