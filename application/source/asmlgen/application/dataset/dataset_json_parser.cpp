90312fdb3a790cc43f9099cd299b11ad44ca0ab7
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-26 23:46:01 -0400

    moved code to header because of templating issues

--- application/source/asmlgen/application/dataset/dataset_json_parser.cpp
+++ application/source/asmlgen/application/dataset/dataset_json_parser.cpp
@@ -4,12 +4,6 @@ namespace dataset
 {
 
 template<typename T>
-const std::vector<TransientDatasetEntry>& DatasetJsonParser<T>::Entries() const noexcept
-{
-  return static_cast<std::vector<TransientDatasetEntry>&>(entries_);
-}
-
-template<typename T>
 const std::unordered_map<std::string, std::vector<std::string>>& DatasetJsonParser<T>::ClassNameGroups() const noexcept
 {
   return class_name_groups_;
